7a9689bce88de53ea3e4f30f2b8a47f72fa8f8b6
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-26 16:52:09 -0700

    WIP. SectionMain. add data prototypes

--- src/components/2_main/index.tsx
+++ src/components/2_main/index.tsx
@@ -1,7 +1,59 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
+type ItemType = 'key' | 'pos' | 'delay';
+
+type ItemKey = {
+    type: 'key',
+    char: string;
+};
+
+type ItemPos = {
+    type: 'pos',
+    x: number;
+    y: number;
+};
+
+type ItemDelay = {
+    type: 'delay',
+    n: number;
+};
+
+type Item = ItemKey | ItemPos | ItemDelay;
+
+const items: Item[] = [
+    {
+        type: 'key', char: 'a',
+    },
+    {
+        type: 'key', char: 'a',
+    },
+    {
+        type: 'key', char: 'a',
+    },
+    {
+        type: 'key', char: 'a',
+    },
+];
+
+function List() {
+    return (
+        <div className="">
+            <div className="py-2">Items</div>
+            <div className="px-2 py-1 border-primary-500 border rounded">
+                {items.map((item, idx) =>
+                    <div className="grid grid-cols-[min-content,auto] gap-x-2" key={idx}>
+                        <div className="">{item.type}</div>
+                        <div className="">{'char' in item && item.char}</div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
 export function SectionMain() {
     return (
-        <div>SectionMain</div>
+        <div className="p-4 max-w-xl">
+            <List />
+        </div>
     );
 }
