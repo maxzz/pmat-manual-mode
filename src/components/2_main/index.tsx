cffd450ca63f1779ad8419e548991addb8e3bfd1
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-26 17:11:43 -0700

    WIP. SectionMain. add sections layout

--- src/components/2_main/index.tsx
+++ src/components/2_main/index.tsx
@@ -1,4 +1,6 @@
+import { classNames } from '@/utils';
 import React, { Fragment } from 'react';
+import { IconAdd } from '../ui/icons';
 
 type ItemType = 'key' | 'pos' | 'delay';
 
@@ -35,11 +37,37 @@ const items: Item[] = [
     },
 ];
 
-function List() {
+function ActionRow() {
     return (
-        <div className="">
-            <div className="py-2">Items</div>
-            <div className="px-2 py-1 border-primary-500 border rounded">
+        <div className=""></div>
+    );
+}
+
+function ActionProps() {
+    return (
+        <div className=""></div>
+    );
+}
+
+function ButtonAdd() {
+    return (
+        <button className="p-1 border-primary-500 border rounded">
+            <IconAdd className="w-3 h-3" />
+        </button>
+    );
+}
+
+const boxClasses = "px-2 py-1 border-primary-500 border rounded";
+
+function PanelList() {
+    return (
+        <div className="space-y-1">
+            <div className="h-7 flex items-end justify-between">
+                <div className="">Fill in actions</div>
+                <ButtonAdd />
+            </div>
+
+            <div className={classNames("min-h-[20rem]", boxClasses)}>
                 {items.map((item, idx) =>
                     <div className="grid grid-cols-[min-content,auto] gap-x-2" key={idx}>
                         <div className="">{item.type}</div>
@@ -50,10 +78,33 @@ function List() {
         </div>
     );
 }
+
+function PanelProps() {
+    return (
+        <div className="space-y-1">
+            <div className="h-7 flex items-end justify-between">
+                <div className="">Action properties</div>
+            </div>
+
+            <div className={boxClasses}>
+                {items.map((item, idx) =>
+                    <div className="grid grid-cols-[min-content,auto] gap-x-2" key={idx}>
+                        <div className="">{item.type}</div>
+                        <div className="">{'char' in item && item.char}</div>
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+}
+
 export function SectionMain() {
     return (
-        <div className="p-4 max-w-xl">
-            <List />
+        <div className="p-4 max-w-xl flex flex-col gap-2">
+            <div className="grid grid-cols-2 gap-4">
+                <PanelList />
+                <PanelProps />
+            </div>
         </div>
     );
 }
