4021f7e2371a604e9baf3be32ddf844a593592e6
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-27 20:09:47 -0700

    WIP. PanelList. add RowItem component

--- src/components/2_main/panel-item-list/index.tsx
+++ src/components/2_main/panel-item-list/index.tsx
@@ -6,42 +6,23 @@ import { Title } from "./action-add-item";
 import { Fragment } from "react";
 import { ScrollList } from "./scroll-list";
 
-const selectedItemClasses = "text-primary-800 bg-primary-200";
+const itemClasses = "leading-6 hover:text-red-500 hover:bg-primary-500";
+const selectedItemClasses = "text-primary-800 bg-primary-200 cursor-default";
 
-function RowItem({ part1, part2, idx, selectedIdx }: { part1: string; part2: string; idx: number; selectedIdx: number; }) {
+function RowItem({ part1, part2, idx }: { part1: string; part2: string | number; idx: number; }) {
+    const { selectedIdx } = useSnapshot(clientState);
     return (<>
-        <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>{part1}</div>
-        <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{part2}</div>
+        <div className={classNames(selectedIdx === idx && selectedItemClasses, itemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>{part1}</div>
+        <div className={classNames(selectedIdx === idx && selectedItemClasses, itemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{part2}</div>
     </>);
 }
 
 function RowField({ item, idx }: { item: ScriptItem; idx: number; }) {
-    const { selectedIdx } = useSnapshot(clientState);
     switch (item.type) {
-        case 'field': {
-            return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Field</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.id}</div>
-            </>;
-        }
-        case 'key': {
-            return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Keystroke</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.char}</div>
-            </>;
-        }
-        case 'pos': {
-            return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Position</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.x}</div>
-            </>;
-        }
-        case 'delay': {
-            return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Delay</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.n}</div>
-            </>;
-        }
+        case 'field': return <RowItem part1="Field" part2={item.id} idx={idx} />;
+        case 'key': return <RowItem part1="Keystroke" part2={item.char} idx={idx} />;
+        case 'pos': return <RowItem part1="Position" part2={`x: ${item.x}, y: ${item.x}`} idx={idx} />;
+        case 'delay': return <RowItem part1="Delay" part2={item.n} idx={idx} />;
         default: {
             const really: never = item;
             console.error(really);
@@ -52,7 +33,7 @@ function RowField({ item, idx }: { item: ScriptItem; idx: number; }) {
 export function PanelList() {
     const { scriptItems } = useSnapshot(clientState);
     return (
-        <div className="min-h-[20rem] flex flex-col space-y-1 select-none">
+        <div className="h-full min-h-[20rem] flex flex-col space-y-1 select-none">
             <Title />
 
             <ScrollList>
