8e42dfa4b532ca7c8e0f536ee2cbfbc3b3be63b8
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-27 17:47:39 -0700

    WIP. PanelList. add shared-styles.tsx

--- src/components/2_main/panel-item-list/action-add-item.tsx
+++ src/components/2_main/panel-item-list/action-add-item.tsx
@@ -1,21 +1,23 @@
-import { IconAdd } from "@/components/ui/icons";
 import { addScriptItem } from "@/store";
+import { focusClasses, menuContentClasses, menuItemClasses } from "@/components/shared-styles";
+import { IconAdd } from "@/components/ui/icons";
+import { classNames } from "@/utils";
 import * as Menu from '@radix-ui/react-dropdown-menu';
 
 function ButtonAdd() {
     return (
         <Menu.Root>
             <Menu.Trigger asChild>
-                <button className="p-1 border-primary-500 border rounded">
+                <button className={classNames("p-1 border-primary-500 border rounded outline-none", focusClasses)}>
                     <IconAdd className="w-3 h-3" />
                 </button>
             </Menu.Trigger>
             <Menu.Portal container={document.getElementById('portal')}>
-                <Menu.Content className={contentClasses} sideOffset={4} side="bottom" align="end">
-                    <Menu.Item className={itemClasses} onClick={() => addScriptItem('key')}>Keystroke</Menu.Item>
-                    <Menu.Item className={itemClasses} onClick={() => addScriptItem('pos')}>Position</Menu.Item>
-                    <Menu.Item className={itemClasses} onClick={() => addScriptItem('field')}>Field</Menu.Item>
-                    <Menu.Item className={itemClasses} onClick={() => addScriptItem('delay')}>Delay</Menu.Item>
+                <Menu.Content className={menuContentClasses} sideOffset={4} side="bottom" align="end">
+                    <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('key')}>Keystroke</Menu.Item>
+                    <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('pos')}>Position</Menu.Item>
+                    <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('field')}>Field</Menu.Item>
+                    <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('delay')}>Delay</Menu.Item>
                 </Menu.Content>
             </Menu.Portal>
         </Menu.Root>
@@ -30,17 +32,3 @@ export function Title() {
         </div>
     );
 }
-
-const contentClasses = "p-1 min-w-[14ch] text-sm \
-text-primary-200 dark:text-primary-800 bg-primary-200 dark:bg-primary-400 \
-data-[side=top]:animate-slideDownAndFade \
-data-[side=right]:animate-slideLeftAndFade \
-data-[side=bottom]:animate-slideUpAndFade \
-data-[side=left]:animate-slideRightAndFade \
-rounded-sm select-none \
-";
-
-const itemClasses = "px-2 py-1 rounded-sm outline-none \
-data-[highlighted]:text-primary-100 data-[highlighted]:bg-primary-700 \
-dark:data-[highlighted]:text-primary-100 dark:data-[highlighted]:bg-primary-700 \
-";
---
+++ src/components/shared-styles.tsx
@@ -0,0 +1,20 @@
+export const focusClasses = "\
+focus:ring-primary-600 dark:focus:ring-primary-400 \
+focus:ring-offset-primary-200 dark:focus:ring-offset-primary-800 \
+focus:ring-1 focus:ring-offset-1 focus:outline-none \
+";
+
+export const menuContentClasses = "p-1 min-w-[14ch] text-sm \
+text-primary-200 dark:text-primary-800 \
+bg-primary-200 dark:bg-primary-300 \
+data-[side=top]:animate-slideDownAndFade \
+data-[side=right]:animate-slideLeftAndFade \
+data-[side=bottom]:animate-slideUpAndFade \
+data-[side=left]:animate-slideRightAndFade \
+rounded-sm select-none \
+";
+
+export const menuItemClasses = "px-2 py-1 rounded-sm outline-none \
+data-[highlighted]:text-primary-100 data-[highlighted]:bg-primary-700 \
+dark:data-[highlighted]:text-primary-100 dark:data-[highlighted]:bg-primary-700 \
+";
