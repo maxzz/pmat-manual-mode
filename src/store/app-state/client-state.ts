5549668693a619555f5b98f9c833dbd27077ddec
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-27 17:38:21 -0700

    WIP. PanelList. add actions can be out of react render tree

--- src/store/app-state/client-state.ts
+++ src/store/app-state/client-state.ts
@@ -1,5 +1,5 @@
 import { proxy } from 'valtio';
-import { ScriptItem } from '../editor-types';
+import { ItemDelay, ItemField, ItemKey, ItemPos, ScriptItem, ScriptItemType } from '../editor-types';
 
 type ClientState = {
     scriptItems: ScriptItem[];
@@ -26,3 +26,45 @@ export const initialItems: ScriptItem[] = [
 export const clientState = proxy<ClientState>({
     scriptItems: [...initialItems],
 });
+
+export function addScriptItem(type: ScriptItemType) {
+    switch (type) {
+        case "field": {
+            const newItem: ItemField = {
+                type: 'field',
+                id: '444',
+            }
+            clientState.scriptItems.push(newItem);
+            break;
+        }
+        case "key": {
+            const newItem: ItemKey = {
+                type: 'key',
+                char: 'Tab',
+            }
+            clientState.scriptItems.push(newItem);
+            break;
+        }
+        case "pos": {
+            const newItem: ItemPos = {
+                type: 'pos',
+                x: 10,
+                y: 20,
+            }
+            clientState.scriptItems.push(newItem);
+            break;
+        }
+        case "delay": {
+            const newItem: ItemDelay = {
+                type: 'delay',
+                n: 1000,
+            }
+            clientState.scriptItems.push(newItem);
+            break;
+        }
+        default: {
+            const really: never = type;
+            console.error(really);
+        }
+    }
+}
