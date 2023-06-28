3aac648b5eb5c7755cc74e9de01704b99901d27b
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-27 19:51:59 -0700

    WIP. PanelList. fix row selection

--- src/components/2_main/panel-item-list/index.tsx
+++ src/components/2_main/panel-item-list/index.tsx
@@ -8,31 +8,38 @@ import { ScrollList } from "./scroll-list";
 
 const selectedItemClasses = "text-primary-800 bg-primary-200";
 
+function RowItem({ part1, part2, idx, selectedIdx }: { part1: string; part2: string; idx: number; selectedIdx: number; }) {
+    return (<>
+        <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>{part1}</div>
+        <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{part2}</div>
+    </>);
+}
+
 function RowField({ item, idx }: { item: ScriptItem; idx: number; }) {
     const { selectedIdx } = useSnapshot(clientState);
     switch (item.type) {
         case 'field': {
             return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>Field</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>{item.id}</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Field</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.id}</div>
             </>;
         }
         case 'key': {
             return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>Keystroke</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>{item.char}</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Keystroke</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.char}</div>
             </>;
         }
         case 'pos': {
             return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>Position</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>{item.x}</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Position</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.x}</div>
             </>;
         }
         case 'delay': {
             return <>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>Delay</div>
-                <div className={classNames(selectedIdx === idx && selectedItemClasses)} onClick={() => { clientState.selectedIdx = idx; }}>{item.n}</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-2")} onClick={() => { clientState.selectedIdx = idx; }}>Delay</div>
+                <div className={classNames(selectedIdx === idx && selectedItemClasses, "pl-8")} onClick={() => { clientState.selectedIdx = idx; }}>{item.n}</div>
             </>;
         }
         default: {
@@ -49,7 +56,7 @@ export function PanelList() {
             <Title />
 
             <ScrollList>
-                <div className={classNames("grid grid-cols-[min-content,auto] gap-x-4", boxClasses)}>
+                <div className={classNames("grid grid-cols-[min-content,auto]", boxClasses)}>
                     {scriptItems.map((item, idx) =>
                         <Fragment key={idx}>
                             <RowField item={item} idx={idx} />
