9baa3ba8f6544895b36374db76845807db14eb90
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-27 18:12:01 -0700

    WIP. store. add script items persistence

--- src/store/app-state/client-data/index.ts
+++ src/store/app-state/client-data/index.ts
@@ -1,14 +1,43 @@
-import { proxy } from 'valtio';
+import { proxy, subscribe } from 'valtio';
 import { ScriptItem } from '@/store/editor-script-types';
 import { initialItems } from './initial-items';
+import { mergeDefaultAndLoaded } from '@/utils';
 
 export * from './add-item';
 export * from './initial-items';
+
+const STORAGE_UI_KEY = 'pmat-manual-mode:data';
+const STORAGE_UI_VER = 'v1';
 
 type ClientState = {
     scriptItems: ScriptItem[];
 };
 
-export const clientState = proxy<ClientState>({
-    scriptItems: [...initialItems],
+export const clientState = proxy<ClientState>(loadUiInitialState());
+
+// Local storage
+
+function loadUiInitialState(): ClientState {
+    let storageData;
+    let storageDataStr = localStorage.getItem(STORAGE_UI_KEY);
+    if (storageDataStr) {
+        try {
+            storageData = JSON.parse(storageDataStr)?.[STORAGE_UI_VER];
+        } catch (error) {
+            console.error('storage bad format');
+        }
+    }
+
+    const initialState: ClientState = {
+        scriptItems: [...initialItems],
+    };
+
+    const ready = mergeDefaultAndLoaded({ defaults: initialState, loaded: storageData });;
+    return ready;
+}
+
+subscribe(clientState, () => {
+    //console.log('store ui  ', appUi.uiState);
+
+    localStorage.setItem(STORAGE_UI_KEY, JSON.stringify({ [STORAGE_UI_VER]: clientState }));
 });
