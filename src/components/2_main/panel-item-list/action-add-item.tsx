f24e658a222f5416b33ece0e2f9b0b3e425c98ac
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-27 17:18:36 -0700

    WIP. PanelList. add dropdown menu animations

--- src/components/2_main/panel-item-list/action-add-item.tsx
+++ src/components/2_main/panel-item-list/action-add-item.tsx
@@ -1,46 +1,6 @@
 import { IconAdd } from "@/components/ui/icons";
 import * as Menu from '@radix-ui/react-dropdown-menu';
 
-const contentClasses = "p-1 min-w-[14ch] text-sm \
-text-primary-200 dark:text-primary-800 bg-primary-200 dark:bg-primary-400 \
-rounded-sm select-none \
-";
-// "\
-// data-[side=top]:animate-slideDownAndFade \
-// data-[side=right]:animate-slideLeftAndFade \
-// data-[side=bottom]:animate-slideUpAndFade \
-// data-[side=left]:animate-slideRightAndFade \
-// "
-
-const itemClasses = "px-2 py-1 rounded-sm outline-none \
-data-[highlighted]:text-primary-100 data-[highlighted]:bg-primary-700 \
-dark:data-[highlighted]:text-primary-100 dark:data-[highlighted]:bg-primary-700 \
-";
-
-// "\
-// group \
-// text-[13px] \
-// leading-none \
-// \
-// text-violet11 \
-// \
-// rounded-[3px] \
-// flex \
-// items-center \
-// \
-// h-[25px] \
-// px-[5px] \
-// relative \
-// pl-[25px] \
-// select-none \
-// outline-none \
-// \
-// data-[disabled]:text-mauve8 \
-// data-[disabled]:pointer-events-none \
-// data-[highlighted]:bg-violet9 \
-// data-[highlighted]:text-violet1 \
-// "
-
 function ButtonAdd() {
     return (
         <Menu.Root>
@@ -69,3 +29,17 @@ export function Title() {
         </div>
     );
 }
+
+const contentClasses = "p-1 min-w-[14ch] text-sm \
+text-primary-200 dark:text-primary-800 bg-primary-200 dark:bg-primary-400 \
+data-[side=top]:animate-slideDownAndFade \
+data-[side=right]:animate-slideLeftAndFade \
+data-[side=bottom]:animate-slideUpAndFade \
+data-[side=left]:animate-slideRightAndFade \
+rounded-sm select-none \
+";
+
+const itemClasses = "px-2 py-1 rounded-sm outline-none \
+data-[highlighted]:text-primary-100 data-[highlighted]:bg-primary-700 \
+dark:data-[highlighted]:text-primary-100 dark:data-[highlighted]:bg-primary-700 \
+";
