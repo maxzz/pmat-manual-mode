c72ce8ed84379bbfc4fdea92227aadf447913a3c
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-28 20:15:39 -0700

    WIP. PanelList. update title dark/light modes

---
+++ src/components/2_main/panel-item-list/title.tsx
@@ -0,0 +1,83 @@
+import { ScriptItemType, addScriptItem } from "@/store";
+import { focusClasses, menuContentClasses, menuItemClasses } from "@/components/shared-styles";
+import { IconAdd, IconDelay, IconField, IconKey, IconPos } from "@/components/ui/icons";
+import { classNames } from "@/utils";
+import * as Menu from '@radix-ui/react-dropdown-menu';
+import { ReactNode } from "react";
+
+function rowText(type: ScriptItemType): { name: string; icon: ReactNode; } {
+    switch (type) {
+        case 'field': /**/ return { /**/ name: "Field"     /**/, icon: <IconField /**/ className="ml-2 w-4 h-4" /> };
+        case 'key':   /**/ return { /**/ name: "Keystroke" /**/, icon: <IconKey   /**/ className="ml-2 w-4 h-4" /> };
+        case 'pos':   /**/ return { /**/ name: "Position"  /**/, icon: <IconPos   /**/ className="ml-2 mt-1 w-4 h-4" /> };
+        case 'delay': /**/ return { /**/ name: "Delay"     /**/, icon: <IconDelay /**/ className="ml-2 w-4 h-4" /> };
+        default: {
+            const really: never = type;
+            return { icon: null, name: '' };
+        }
+    }
+}
+
+function MenuRow({ type }: { type: ScriptItemType; }) {
+    const { name, icon } = rowText(type);
+    return (
+        <Menu.Item className={classNames(menuItemClasses, "grid grid-cols-[auto,1fr] gap-x-2 items-center")} onClick={() => addScriptItem(type)}>
+            {icon}
+            <div className="">{name}</div>
+        </Menu.Item>
+    );
+}
+
+function ButtonAdd() {
+    return (
+        <Menu.Root>
+            <Menu.Trigger asChild>
+                <button className={classNames("p-1 border-primary-500 border rounded shadow outline-none", focusClasses)}>
+                    <IconAdd className="w-3 h-3" />
+                </button>
+            </Menu.Trigger>
+            <Menu.Portal container={document.getElementById('portal')}>
+                <Menu.Content className={menuContentClasses} sideOffset={1} alignOffset={-8} side="bottom" align="end">
+                    <MenuRow type="key" />
+                    <MenuRow type="pos" />
+                    <MenuRow type="field" />
+                    <MenuRow type="delay" />
+                </Menu.Content>
+            </Menu.Portal>
+        </Menu.Root>
+    );
+}
+
+// function ButtonAdd() {
+//     return (
+//         <Menu.Root>
+//             <Menu.Trigger asChild>
+//                 <button className={classNames("p-1 border-primary-500 border rounded outline-none", focusClasses)}>
+//                     <IconAdd className="w-3 h-3" />
+//                 </button>
+//             </Menu.Trigger>
+//             <Menu.Portal container={document.getElementById('portal')}>
+//                 <Menu.Content className={menuContentClasses} sideOffset={1} alignOffset={-8} side="bottom" align="end">
+//                     <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('key')}>Keystroke</Menu.Item>
+//                     <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('pos')}>Position</Menu.Item>
+//                     <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('field')}>Field</Menu.Item>
+//                     <Menu.Item className={menuItemClasses} onClick={() => addScriptItem('delay')}>Delay</Menu.Item>
+//                 </Menu.Content>
+//             </Menu.Portal>
+//         </Menu.Root>
+//     );
+// }
+
+export function Title() {
+    return (
+        <div className="h-7 flex items-end justify-between">
+            <div className="">Fill in actions</div>
+            <ButtonAdd />
+            {/* <IconPos className="w-4 h-4" />
+            <IconField className="w-4 h-4" />
+            <IconKey className="w-4 h-4" />
+            <IconDelay className="w-4 h-4" />
+            <IconCode className="w-4 h-4" /> */}
+        </div>
+    );
+}
--- src/components/shared-styles.tsx
+++ src/components/shared-styles.tsx
@@ -8,10 +8,10 @@ focus:ring-1 focus:ring-offset-1 focus:outline-none";
 
 export const menuContentClasses = "p-1 min-w-[14ch] text-sm \
 text-primary-800 \
-dark:text-primary-800 \
+dark:text-primary-200 \
 \
 bg-primary-100 \
-dark:bg-primary-300 \
+dark:bg-primary-800 \
 \
 data-[side=top]:animate-slideDownAndFade \
 data-[side=right]:animate-slideLeftAndFade \
@@ -19,12 +19,12 @@ data-[side=bottom]:animate-slideUpAndFade \
 data-[side=left]:animate-slideRightAndFade \
 \
 border-primary-500 \
-dark:border-primary-200 \
+dark:border-primary-400 \
 dark:shadow-primary-500 \
 \
 border rounded shadow select-none";
 
-export const menuItemClasses = "px-2 py-1 rounded-sm outline-none \
+export const menuItemClasses = "pr-3 py-1 rounded-sm outline-none \
 data-[highlighted]:text-primary-800 \
 dark:data-[highlighted]:text-primary-100 \
 \
