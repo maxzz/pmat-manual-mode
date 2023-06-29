d8337a26b39e6899d49d0a861eed2c21ae80e8de
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-28 18:47:52 -0700

    WIP. PanelList. update icons outlines

--- src/components/2_main/index.tsx
+++ src/components/2_main/index.tsx
@@ -6,7 +6,7 @@ export const boxClasses = "px-2 py-1 border-primary-500 border rounded";
 export function SectionMain() {
     return (
         <div className="p-4 max-w-xl flex flex-col gap-2">
-            <div className="grid grid-cols-2 gap-4">
+            <div className="h-full grid grid-cols-2 gap-4">
                 <PanelList />
                 <PanelProps />
             </div>
