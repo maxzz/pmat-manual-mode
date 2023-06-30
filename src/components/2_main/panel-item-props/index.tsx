96721a2b117302e860b3864991dba541dbed3821
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-30 15:29:23 -0700

    WIP. PanelProps. add item-props.tsx

--- src/components/2_main/panel-item-props/index.tsx
+++ src/components/2_main/panel-item-props/index.tsx
@@ -5,77 +5,19 @@ import { editorFrameClasses, focusWithinClasses } from "../../shared-styles";
 import { classNames } from "@/utils";
 import { IconField, IconKey, IconPos, IconDelay } from "@/components/ui/icons";
 import { Title } from "./title";
+import { getPropsView } from "./item-props";
 
-function ActionProps() {
-    return (
-        <div className=""></div>
-    );
-}
-
-function rowText(item: ScriptItem): { name: string; icon: ReactNode; details: string; } {
+function itemNameAndIcon(item: ScriptItem): { name: string; icon: ReactNode; } {
     switch (item.type) {
-        case 'field': /**/ return { /**/ name: "Field"     /**/, icon: <IconField /**/ className="ml-2 w-4 h-4" />, details: `${item.id}` };
-        case 'key':   /**/ return { /**/ name: "Keystroke" /**/, icon: <IconKey   /**/ className="ml-2 w-4 h-4" />, details: `${item.char}` };
-        case 'pos':   /**/ return { /**/ name: "Position"  /**/, icon: <IconPos   /**/ className="ml-2 mt-1 w-4 h-4" />, details: `${`x: ${item.x}, y: ${item.x}`}` };
-        case 'delay': /**/ return { /**/ name: "Delay"     /**/, icon: <IconDelay /**/ className="ml-2 w-4 h-4" />, details: `${item.n}` };
+        case 'field': /**/ return { /**/ name: "Field"     /**/, icon: <IconField /**/ className="ml-2 w-4 h-4" />, };
+        case 'key':   /**/ return { /**/ name: "Keystroke" /**/, icon: <IconKey   /**/ className="ml-2 w-4 h-4" />, };
+        case 'pos':   /**/ return { /**/ name: "Position"  /**/, icon: <IconPos   /**/ className="ml-2 mt-1 w-4 h-4" />, };
+        case 'delay': /**/ return { /**/ name: "Delay"     /**/, icon: <IconDelay /**/ className="ml-2 w-4 h-4" />, };
         default: {
             const really: never = item;
-            return { icon: null, name: '', details: '' };
+            return { icon: null, name: '', };
         }
     }
-}
-
-function InputField({ label, ...rest }: { label: string; } & InputHTMLAttributes<HTMLInputElement>) {
-    return (
-        <label className="flex flex-col">
-            <div className="text-xs">{label}</div>
-            <input className="px-2 py-1 bg-primary-700/50 rounded" {...rest} />
-        </label>
-    );
-}
-
-function PropsField({ item, ...rest }: { item: ItemField; } & HTMLAttributes<HTMLElement>) {
-    const snap = useSnapshot(item);
-    return (
-        <div className="space-y-2" {...rest}>
-            <InputField label="Field label" value={`${snap.id}`} onChange={(e) => item.id = e.target.value} />
-            <InputField label="Type" />
-            <InputField label="Reference" />
-            <InputField label="Value" />
-        </div>
-    );
-}
-
-function PropsKey({ item, ...rest }: { item: ItemKey; } & HTMLAttributes<HTMLElement>) {
-    const snap = useSnapshot(item);
-    return (
-        <div className="space-y-2" {...rest}>
-            <InputField label="Key" value={`${snap.char}`} onChange={(e) => item.char = e.target.value} />
-            <InputField label="Repeat" />
-            <InputField label="Shift" />
-            <InputField label="Control" />
-            <InputField label="Alt" />
-        </div>
-    );
-}
-
-function PropsPos({ item, ...rest }: { item: ItemPos; } & HTMLAttributes<HTMLElement>) {
-    const snap = useSnapshot(item);
-    return (
-        <div className="space-y-2" {...rest}>
-            <InputField label="x" value={`${snap.x}`} onChange={(e) => item.x = parseInt(e.target.value)} />
-            <InputField label="y" value={`${snap.y}`} onChange={(e) => item.y = parseInt(e.target.value)} />
-        </div>
-    );
-}
-
-function PropsDelay({ item, ...rest }: { item: ItemDelay; } & HTMLAttributes<HTMLElement>) {
-    const snap = useSnapshot(item);
-    return (
-        <div className="space-y-2" {...rest}>
-            <InputField label="Delay in ms" value={`${snap.n}`} onChange={(e) => item.n = parseInt(e.target.value)} />
-        </div>
-    );
 }
 
 function ItemProps({ idx }: { idx: number; }) {
@@ -85,26 +27,13 @@ function ItemProps({ idx }: { idx: number; }) {
         return null;
     }
     const item = clientState.scriptItems[idx];
-
-    let Comp: ReactNode | null = null;
-    switch (snap.type) {
-        case 'field': /**/ { Comp = <PropsField item={item as ItemField} />; break; }
-        case 'key':   /**/ { Comp = <PropsKey item={item as ItemKey} />; break; }
-        case 'pos':   /**/ { Comp = <PropsPos item={item as ItemPos} />; break; }
-        case 'delay': /**/ { Comp = <PropsDelay item={item as ItemDelay} />; break; }
-        default: {
-            const really: never = snap;
-            Comp = null;
-        }
-    }
-
-    const title = rowText(snap);
-
+    const Comp: ReactNode = getPropsView({ snap, item });
+    const { name, icon } = itemNameAndIcon(snap);
     return (
         <div className="">
             <div className="mt-1 mb-4 font-semibold flex items-center justify-between">
-                <div className="">{title.name}</div>
-                {title.icon}
+                <div className="">{name}</div>
+                {icon}
             </div>
 
             {Comp && Comp}
---
+++ src/components/2_main/panel-item-props/item-props.tsx
@@ -0,0 +1,71 @@
+import { InputHTMLAttributes, HTMLAttributes, ReactNode } from "react";
+import { useSnapshot } from "valtio";
+import { ItemField, ItemKey, ItemPos, ItemDelay, ScriptItem } from "@/store";
+
+function InputField({ label, ...rest }: { label: string; } & InputHTMLAttributes<HTMLInputElement>) {
+    return (
+        <label className="flex flex-col">
+            <div className="ml-2 text-xs">{label}</div>
+            <input className="px-2 py-1 bg-primary-700/50 rounded" {...rest} />
+        </label>
+    );
+}
+
+function PropsField({ item, ...rest }: { item: ItemField; } & HTMLAttributes<HTMLElement>) {
+    const snap = useSnapshot(item);
+    return (
+        <div className="px-4 space-y-2" {...rest}>
+            <InputField label="Field label" value={`${snap.id}`} onChange={(e) => item.id = e.target.value} />
+            <InputField label="Type" />
+            <InputField label="Reference" />
+            <InputField label="Value" />
+        </div>
+    );
+}
+
+function PropsKey({ item, ...rest }: { item: ItemKey; } & HTMLAttributes<HTMLElement>) {
+    const snap = useSnapshot(item);
+    return (
+        <div className="px-4 space-y-2" {...rest}>
+            <InputField label="Key" value={`${snap.char}`} onChange={(e) => item.char = e.target.value} />
+            <InputField label="Repeat" />
+            <InputField label="Shift" />
+            <InputField label="Control" />
+            <InputField label="Alt" />
+        </div>
+    );
+}
+
+function PropsPos({ item, ...rest }: { item: ItemPos; } & HTMLAttributes<HTMLElement>) {
+    const snap = useSnapshot(item);
+    return (
+        <div className="px-4 space-y-2" {...rest}>
+            <InputField label="x" value={`${snap.x}`} onChange={(e) => item.x = parseInt(e.target.value)} />
+            <InputField label="y" value={`${snap.y}`} onChange={(e) => item.y = parseInt(e.target.value)} />
+        </div>
+    );
+}
+
+function PropsDelay({ item, ...rest }: { item: ItemDelay; } & HTMLAttributes<HTMLElement>) {
+    const snap = useSnapshot(item);
+    return (
+        <div className="px-4 space-y-2" {...rest}>
+            <InputField label="Delay in ms" value={`${snap.n}`} onChange={(e) => item.n = parseInt(e.target.value)} />
+        </div>
+    );
+}
+
+export function getPropsView({ snap, item }: { snap: ScriptItem; item: ScriptItem; }): ReactNode {
+    let rv: ReactNode | null = null;
+    switch (snap.type) {
+        case 'field': /**/ { rv = <PropsField item={item as ItemField} />; break; }
+        case 'key':   /**/ { rv = <PropsKey item={item as ItemKey} />; break; }
+        case 'pos':   /**/ { rv = <PropsPos item={item as ItemPos} />; break; }
+        case 'delay': /**/ { rv = <PropsDelay item={item as ItemDelay} />; break; }
+        default: {
+            const really: never = snap;
+            rv = null;
+        }
+    }
+    return rv;
+}
