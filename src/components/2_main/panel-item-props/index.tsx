c5b8b5c0305a1c4343e9cfd953cf7488d6c17074
Max Zakharzhevskiy <maxzz@msn.com>
2023-06-30 13:47:05 -0700

    WIP. PanelProps. fix container

--- src/components/2_main/panel-item-props/index.tsx
+++ src/components/2_main/panel-item-props/index.tsx
@@ -1,6 +1,9 @@
+import { ReactNode } from "react";
 import { useSnapshot } from "valtio";
-import { clientState } from "@/store";
-import { editorFrameClasses } from "../../shared-styles";
+import { ScriptItem, clientState, editorState } from "@/store";
+import { editorFrameClasses, focusClasses } from "../../shared-styles";
+import { classNames } from "@/utils";
+import { IconField, IconKey, IconPos, IconDelay } from "@/components/ui/icons";
 
 function ActionProps() {
     return (
@@ -8,26 +11,42 @@ function ActionProps() {
     );
 }
 
-export function PanelProps() {
+function rowText(item: ScriptItem): { name: string; icon: ReactNode; details: string; } {
+    switch (item.type) {
+        case 'field': /**/ return { /**/ name: "Field"     /**/, icon: <IconField /**/ className="ml-2 w-4 h-4" />, details: `${item.id}` };
+        case 'key':   /**/ return { /**/ name: "Keystroke" /**/, icon: <IconKey   /**/ className="ml-2 w-4 h-4" />, details: `${item.char}` };
+        case 'pos':   /**/ return { /**/ name: "Position"  /**/, icon: <IconPos   /**/ className="ml-2 mt-1 w-4 h-4" />, details: `${`x: ${item.x}, y: ${item.x}`}` };
+        case 'delay': /**/ return { /**/ name: "Delay"     /**/, icon: <IconDelay /**/ className="ml-2 w-4 h-4" />, details: `${item.n}` };
+        default: {
+            const really: never = item;
+            return { icon: null, name: '', details: '' };
+        }
+    }
+}
+
+function ItemProps({ idx }: { idx: number; }) {
     const { scriptItems } = useSnapshot(clientState);
+    const snap = scriptItems[idx];
+    if (!snap) {
+        return null;
+    }
+    return (
+        <div className="">
+            {snap.type}
+        </div>
+    );
+}
+
+export function PanelProps() {
+    const { selectedIdx } = useSnapshot(editorState);
     return (
         <div className="space-y-1 select-none">
             <div className="h-7 flex items-end justify-between">
                 <div className="">Action properties</div>
             </div>
 
-            <div className={editorFrameClasses}>
-                {scriptItems.map((item, idx) => {
-                    if (!item) {
-                        return null;
-                    }
-                    return (
-                        <div className="grid grid-cols-[min-content,auto] gap-x-2" key={idx}>
-                            <div className="">{item.type}</div>
-                            <div className="">{'char' in item && item.char}</div>
-                        </div>);
-                }
-                )}
+            <div className={classNames("min-h-[20rem]", editorFrameClasses, focusClasses)} tabIndex={0}>
+                <ItemProps idx={selectedIdx} />
             </div>
         </div>
     );
