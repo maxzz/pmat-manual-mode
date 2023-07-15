d75cc88ffe22c4e857af7ee6267dc205b3919061
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-14 20:02:10 -0700

    WIP. PropsKey. add select item styles

--- src/components/2_main/panel-item-props/item-props/panel-key/select.tsx
+++ src/components/2_main/panel-item-props/item-props/panel-key/select.tsx
@@ -1,29 +1,34 @@
+import { IconChevronDown, IconDot } from '@/components/ui/icons';
 import * as S from '@radix-ui/react-select';
+import { ReactNode } from 'react';
+
+function Item({ value, children }: { value: string; children: ReactNode; }) {
+    return (
+        <S.Item value="1">
+            <S.ItemText>
+                {children}
+            </S.ItemText>
+            <S.ItemIndicator>
+                <IconDot className="w-4 h-4" />
+            </S.ItemIndicator>
+        </S.Item>
+    );
+}
 
 export function SelectOne({ items }: { items: string[]; }) {
     return (
         <S.Root>
-            <S.Trigger className="">
+            <S.Trigger className="flex items-center space-x-2">
                 <S.Value placeholder="select" />
+                <S.Icon><IconChevronDown className="w-4 h-4" /> </S.Icon>
             </S.Trigger>
 
             <S.Portal>
                 <S.Content className="w-10 h-10">
                     <S.Viewport>
-                        <S.Item value="1">
-                            <S.ItemText>
-                                11
-                            </S.ItemText>
-                            <S.ItemText>
-                                12
-                            </S.ItemText>
-                            <S.ItemText>
-                                13
-                            </S.ItemText>
-                            <S.ItemText>
-                                15
-                            </S.ItemText>
-                        </S.Item>
+                        <Item value="1">1</Item>
+                        <Item value="2">2</Item>
+                        <Item value="3">3</Item>
                     </S.Viewport>
                 </S.Content>
             </S.Portal>
