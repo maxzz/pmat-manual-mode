0bb157f76693470da21dacf033cc66ab51631560
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-15 20:06:06 -0700

    WIP. PropsKey. select. fix scroll button styles

--- src/components/2_main/panel-item-props/item-props/panel-key/select.tsx
+++ src/components/2_main/panel-item-props/item-props/panel-key/select.tsx
@@ -20,13 +20,11 @@ data-[highlighted]:outline-none \
 data-[highlighted]:bg-violet9 \
 data-[highlighted]:text-violet1';
 
-//focusClasses
 const itemClasses = "data-[highlighted]:text-red-300 data-[highlighted]:bg-green-200 data-[highlighted]:outline-none";
-const item2Classes = "border-transparent focus:border-transparent focus:ring-0";
 
 const Item = forwardRef(({ value, children }: { value: string; children: ReactNode; }, forwardRef: ForwardedRef<HTMLDivElement>) => {
     return (
-        <S.Item className={`relative h-7 pr-8 pl-6 bg-primary-700 flex items-center ${itemClasses}`} value={value} ref={forwardRef}>
+        <S.Item className={`relative h-7 pr-8 pl-6 bg-primary-800 flex items-center ${itemClasses}`} value={value} ref={forwardRef}>
             <S.ItemText className="">
                 {children}
             </S.ItemText>
@@ -36,6 +34,8 @@ const Item = forwardRef(({ value, children }: { value: string; children: ReactNo
         </S.Item>
     );
 });
+
+const scrollButtonClasses = "h-4 text-primary-200 bg-primary-700 flex items-center justify-center";
 
 export function SelectOne({ items }: { items: string[]; }) {
     return (
@@ -48,18 +48,18 @@ export function SelectOne({ items }: { items: string[]; }) {
             <S.Portal>
                 <S.Content className="text-xs text-primary-200 border-primary-500 border rounded-md overflow-hidden select-none">
 
-                    <S.ScrollUpButton className="h-8 text-primary-200 bg-primary-700 flex items-center justify-center">
-                        <IconChevronUp className="w-4 h-4" />
+                    <S.ScrollUpButton className={scrollButtonClasses}>
+                        <IconChevronUp className="w-3 h-3" />
                     </S.ScrollUpButton>
 
-                    <S.Viewport className="px-4 bg-primary-700">
+                    <S.Viewport className="px-4 bg-primary-800">
                         {items.map((item) => (
                             <Item value={item} key={item}>{item}</Item>
                         ))}
                     </S.Viewport>
 
-                    <S.ScrollDownButton className="h-8 text-primary-200 bg-primary-700 flex items-center justify-center">
-                        <IconChevronDown className="w-4 h-4" />
+                    <S.ScrollDownButton className={scrollButtonClasses}>
+                        <IconChevronDown className="w-3 h-3" />
                     </S.ScrollDownButton>
 
                 </S.Content>
