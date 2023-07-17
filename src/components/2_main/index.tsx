dad12f4ac6913b38695680b786c15780b93b0c1d
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-16 20:10:53 -0700

    WIP. SectionMain. update size responsiveness

--- src/components/2_main/index.tsx
+++ src/components/2_main/index.tsx
@@ -4,7 +4,7 @@ import { PanelProps } from './panel-item-props';
 export function SectionMain() {
     return (
         <div className="p-4 max-w-[720px] flex flex-col gap-2">
-            <div className="h-full grid grid-cols-2 gap-4">
+            <div className="h-full grid grid-cols-[minmax(330px,1fr),minmax(200px,1fr)] gap-4">
                 <PanelList />
                 <PanelProps />
             </div>
