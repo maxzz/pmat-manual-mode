a1126b0d17efd4f14972d82a0c27b8d9d0d26cc5
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-16 19:54:24 -0700

    WIP. PropsKey. select. house keeping

--- src/components/2_main/panel-item-props/item-props/panel-key/select.tsx
+++ src/components/2_main/panel-item-props/item-props/panel-key/select.tsx
@@ -33,7 +33,7 @@ data-[highlighted]:outline-primary-500 \
 const Item = forwardRef(({ value, children }: { value: string; children: ReactNode; }, forwardRef: ForwardedRef<HTMLDivElement>) => {
     return (
         <S.Item className={classNames("rounded", itemClasses)} value={value} ref={forwardRef}>
-            <div className={classNames("relative h-7 pr-8 pl-6 py-2 flex items-center")}>
+            <div className={classNames("relative h-6 pr-8 pl-6 py-2 flex items-center")}>
                 <S.ItemText className="text-[.55rem] h-6">
                     {children}
                 </S.ItemText>
@@ -45,24 +45,26 @@ const Item = forwardRef(({ value, children }: { value: string; children: ReactNo
     );
 });
 
+const triggerClasses = "w-full px-2 py-1 border-primary-500 border rounded flex items-center justify-between";
+const contentClasses = "text-xs text-primary-300 bg-primary-900 border-primary-500 border rounded-md shadow shadow-primary-500 overflow-hidden select-none";
 const scrollButtonClasses = "h-4 text-primary-200 bg-primary-900 flex items-center justify-center";
 
 export function SelectOne({ items }: { items: string[]; }) {
     return (
         <S.Root>
-            <S.Trigger className={classNames("w-full px-2 py-1 border-primary-500 border rounded flex items-center justify-between", focusClasses)}>
+            <S.Trigger className={classNames(triggerClasses, focusClasses)}>
                 <S.Value placeholder="select" />
-                <S.Icon><IconChevronDown className="w-3 h-" /> </S.Icon>
+                <S.Icon><IconChevronDown className="w-3 h-3" /> </S.Icon>
             </S.Trigger>
 
             <S.Portal>
-                <S.Content className="text-xs text-primary-300 border-primary-500 border rounded-md shadow shadow-primary-500 overflow-hidden select-none">
+                <S.Content className={contentClasses}>
 
                     <S.ScrollUpButton className={scrollButtonClasses}>
                         <IconChevronUp className="w-3 h-3" />
                     </S.ScrollUpButton>
 
-                    <S.Viewport className="p-2 bg-primary-900">
+                    <S.Viewport className="p-2">
                         {items.map((item) => (
                             <Item value={item} key={item}>{item}</Item>
                         ))}
