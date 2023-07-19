24f1543aaea46badb1c2f0ee9817eb419e5363ff
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-18 17:21:16 -0700

    WIP. PropsPos. enlarge custom cursor size

--- tailwind/cursors.js
+++ tailwind/cursors.js
@@ -1,7 +1,7 @@
 const SVG = require('mini-svg-data-uri');
 
 const cursorTarget = `url("${SVG(`
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="white" stroke-linecap="round" viewBox="0 0 24 24">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="white" stroke-linecap="round" width="64" height="64" viewBox="0 0 24 24">
   <circle cx="12" cy="12" r="8.04"/>
   <circle cx="12" cy="12" r="4.99"/>
   <path d="M12 1.5v8.13"/>
@@ -9,7 +9,7 @@ const cursorTarget = `url("${SVG(`
   <path d="M12 22.5v-8.13"/>
   <path d="M1.5 12h8.13"/>
 </svg>
-`)}") 12 12, auto`;
+`)}") 32 32, auto`;
 
 module.exports = {
     cursorTarget,
