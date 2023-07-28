48e8ce643672111a085ad100416a3a450d157410
Max Zakharzhevskiy <maxzz@msn.com>
2023-07-27 20:04:22 -0700

    WIP. icons. add search svg

---
+++ src/components/ui/icons/normal/icon31-search.tsx
@@ -0,0 +1,12 @@
+import { HTMLAttributes, SVGAttributes } from 'react';
+import { classNames } from '@/utils';
+
+export function IconSearch({ className, title, ...rest }: HTMLAttributes<SVGSVGElement> & SVGAttributes<SVGSVGElement>) { // pkg-size.dev
+    return (
+        <svg viewBox="0 0 24 24" className={classNames("fill-none stroke-current stroke-[1.5]", className)} strokeLinecap="round" strokeLinejoin="round" {...rest}>
+            {title && <title>{title}</title>}
+            <circle cx="11" cy="11" r="8" />
+            <path d="m21 21l-4.3-4.3" />
+        </svg>
+    );
+}
