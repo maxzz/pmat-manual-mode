e5b3f1b0ee29cb84e51b7e95ab44bf32c6f1f015
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-03 18:25:50 -0700

    WIP. field list. fix input light schema

--- src/components/2_main/panel-item-props/ui/input-field.tsx
+++ src/components/2_main/panel-item-props/ui/input-field.tsx
@@ -2,11 +2,16 @@ import { InputHTMLAttributes } from "react";
 import { classNames } from "@/utils";
 import { focusClasses } from "@/components/shared-styles";
 
+const inputTextClasses = "px-2 py-1 \
+border-primary-400 border dark:border-none \
+bg-primary-100 dark:bg-primary-700/50 rounded \
+";
+
 export function InputField({ label, horizontal = false, className, labelClasses, ...rest }: { label: string; horizontal?: boolean; labelClasses?: string; } & InputHTMLAttributes<HTMLInputElement>) {
     return (
         <label className={classNames("flex", horizontal ? "items-center space-x-2" : "flex-col space-y-1")}>
             <div className={classNames("text-xs", labelClasses)}>{label}</div>
-            <input className={classNames("px-2 py-1 bg-primary-200 dark:bg-primary-700/50 rounded", focusClasses, className)} {...rest} />
+            <input className={classNames(inputTextClasses, focusClasses, className)} {...rest} />
         </label>
     );
 }
