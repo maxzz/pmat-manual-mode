fd9726b1597eca6d71c0e0492e5251ef7b7b632f
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-03 20:26:01 -0700

    WIP. field list. add rowDetails component

--- src/components/2_main/panel-script-item-list/index.tsx
+++ src/components/2_main/panel-script-item-list/index.tsx
@@ -1,48 +1,16 @@
-import { ReactNode } from "react";
 import { useSnapshot } from "valtio";
-import { clientState, editorState, moveScriptCursor, removeScriptItem, ScriptItem, SrcriptItemDelay, SrcriptItemField, SrcriptItemKey, SrcriptItemPos, swapScriptItems } from "@/store";
-import { classNames, plural } from "@/utils";
+import { clientState, editorState, moveScriptCursor, removeScriptItem, ScriptItem, swapScriptItems } from "@/store";
+import { classNames } from "@/utils";
 import { editorFrameClasses, focusClasses } from "../../shared-styles";
 import { Title } from "./panel-title";
 import { ScrollList } from "./scroll-list";
-import { IconField, IconKey, IconPos, IconDelay } from "@/components/ui/icons";
 import { rowClasses, rowSelectedClasses } from "@/components/shared-styles";
 import { MenuState, RowMenuButton } from "./row-popup-menu";
-
-const detailKeyClasses = "px-1 py-px min-w-[1.5rem] text-[.55rem] leading-4 text-center \
-bg-gradient-to-b \
-from-primary-200 to-primary-300 \
-dark:from-primary-600 dark:to-primary-700 dark:shadow-primary-300/20 \
-shadow \
-dark:shadow-inner \
-border-primary-400 \
-dark:border-primary-800 \
-border rounded-sm \
-";
-
-const detailsFld = (item: SrcriptItemField) => `${item.id}`;
-const detailsPos = (item: SrcriptItemPos) => `${`x: ${item.x}, y: ${item.y}`}`;
-const detailsDly = (item: SrcriptItemDelay) => `${item.n}`;
-const detailsKey = (item: SrcriptItemKey) => (<div className="flex items-center space-x-1">
-    <div className={detailKeyClasses}>{item.char}</div> <div>{item.repeat} {plural(item.repeat, 'time')}</div>
-</div>);
-
-function rowText(item: ScriptItem): { name: string; icon: ReactNode; details: ReactNode; } {
-    switch (item.type) {
-        case 'field': /**/ return { /**/ name: "Field"     /**/, icon: <IconField /**/ className="ml-2 w-4 h-4" />, details: detailsFld(item) };
-        case 'key':   /**/ return { /**/ name: "Keystroke" /**/, icon: <IconKey   /**/ className="ml-2 w-4 h-4" />, details: detailsKey(item) };
-        case 'pos':   /**/ return { /**/ name: "Position"  /**/, icon: <IconPos   /**/ className="ml-2 mt-1 w-4 h-4" />, details: detailsPos(item) };
-        case 'delay': /**/ return { /**/ name: "Delay"     /**/, icon: <IconDelay /**/ className="ml-2 w-4 h-4" />, details: detailsDly(item) };
-        default: {
-            const really: never = item;
-            return { icon: null, name: '', details: '' };
-        }
-    }
-}
+import { rowDetails } from "./row-details";
 
 function RowFieldCompound({ item, idx, menuState }: { item: ScriptItem; idx: number; menuState: MenuState; }) {
     const { selectedIdx } = useSnapshot(editorState);
-    const { icon, name, details } = rowText(item);
+    const { icon, name, details } = rowDetails(item);
     return (
         <div
             className={classNames("py-0.5 grid grid-cols-[min-content,5rem,1fr,min-content] items-center", rowClasses, selectedIdx === idx && rowSelectedClasses)}
