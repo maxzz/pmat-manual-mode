e849b4bb4c373ab4f113b23bbf773360f873b261
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-04 19:35:38 -0700

    WIP. controls. split shared-styles

---
+++ src/components/ui/shared-styles/index.tsx
@@ -0,0 +1,22 @@
+export * from './menu';
+export * from './actions-list';
+
+export const focusClasses = "\
+focus:ring-primary-600 \
+dark:focus:ring-primary-400 \
+focus:ring-offset-primary-200 \
+dark:focus:ring-offset-primary-800 \
+\
+focus:ring-1 focus:ring-offset-1 focus:outline-none";
+
+export const focusWithinClasses = "\
+focus-within:ring-primary-600 \
+dark:focus-within:ring-primary-400 \
+focus-within:ring-offset-primary-200 \
+dark:focus-within:ring-offset-primary-800 \
+\
+focus-within:ring-1 focus-within:ring-offset-1 focus-within:outline-none";
+
+export const editorFrameClasses = "px-2 py-1 border-primary-500 border rounded";
+
+export const gropuTitleClasses = "font-semibold text-yellow-800 dark:text-yellow-300";
