87611017670615c3fdf27ac4712d177f75f24d3b
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-04 19:49:21 -0700

    WIP. controls. update main panels layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import { SectionHeader } from "./components/1_header";
-import { SectionMain } from "./components/2_main";
-import { SectionFooter } from "./components/3_footer";
+import { SectionHeader } from "./components/1-header";
+import { SectionMain } from "./components/2-main";
+import { SectionFooter } from "./components/3-footer";
 
 const appColorClasses = "text-primary-700 dark:text-primary-100 bg-primary-100 dark:bg-primary-800";
 
