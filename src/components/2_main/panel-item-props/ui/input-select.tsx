cce571052fa017ba68bc6fa20f2a7319ea723ecd
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-04 19:14:14 -0700

    WIP. controls. fix select classnames propagation

--- src/components/2_main/panel-item-props/ui/input-select.tsx
+++ src/components/2_main/panel-item-props/ui/input-select.tsx
@@ -5,7 +5,7 @@ import { SelectItemText } from "@/store";
 
 export function InputSelect({ items, label, labelClasses, title, horizontal = false, className, ...rest }: { items: SelectItemText[]; label: string; labelClasses?: string; horizontal?: boolean; } & StringValueChange & InputHTMLAttributes<HTMLInputElement>) {
     return (
-        <div className={classNames("flex", horizontal ? "items-center space-x-2" : "flex-col space-y-1")} title={title}>
+        <div className={classNames("flex", horizontal ? "items-center space-x-2" : "flex-col space-y-1", className)} title={title}>
             <div className={classNames("text-xs", labelClasses)}>{label}</div>
             <SelectOne items={items} {...rest} />
         </div>
