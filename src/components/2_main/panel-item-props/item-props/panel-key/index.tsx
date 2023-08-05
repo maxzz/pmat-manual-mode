aebf2da1349ece3ecc33818b9bf058ba1866dc70
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-04 19:19:50 -0700

    WIP. controls. fix modifiers layout

--- src/components/2_main/panel-item-props/item-props/panel-key/index.tsx
+++ src/components/2_main/panel-item-props/item-props/panel-key/index.tsx
@@ -27,7 +27,7 @@ export function InputRepeat({ item }: { item: SrcriptItemKey; }) {
 function Modifier({ label, name, item }: { label: string; name: SrcriptItemModifiers; item: SrcriptItemKey; }) {
     const snap = useSnapshot(item);
     return (
-        <div className={classNames("flex items-center space-x-2")}>
+        <div className={classNames("max-w-[10rem] flex items-center space-x-2")}>
             <div className="min-w-[8ch] text-xs">{label}</div>
             <SelectOne items={modifierKeys} value={`${snap[name]}`} onValueChange={(value) => item[name] = +value} />
             {/* <input className={classNames("px-2 py-1 bg-primary-700/50 rounded", focusClasses)} /> */}
@@ -52,7 +52,7 @@ export function PropsKey({ item, ...rest }: { item: SrcriptItemKey; } & HTMLAttr
             <InputRepeat item={item} />
 
             <div className="-mx-2 pt-3">
-                <div className="p-2 border-primary-700 border rounded flex flex-col space-y-1 relative">
+                <div className="p-2 border-primary-400 dark:border-primary-700 border rounded flex flex-col space-y-1 relative">
                     <div className="absolute left-1 -top-2.5 px-1 bg-primary-100 dark:bg-primary-800">
                         Key modifiers
                     </div>
