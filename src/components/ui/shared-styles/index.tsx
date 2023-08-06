16d43c92efbfb20b2b1e400752ad8fed1e144f8b
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-05 17:57:47 -0700

    WIP. controls. fix actions list selected item

--- src/components/ui/shared-styles/index.tsx
+++ src/components/ui/shared-styles/index.tsx
@@ -2,6 +2,6 @@ export * from './focus';
 export * from './menu';
 export * from './actions-list';
 
-export const editorFrameClasses = "px-2 py-1 border-primary-500 border rounded";
+export const editorFrameClasses = "p-1 border-primary-500 border rounded";
 
 export const gropuTitleClasses = "font-semibold text-yellow-800 dark:text-yellow-300";
