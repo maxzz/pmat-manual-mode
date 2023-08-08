cd45c97e01d53317268a26360c3eb5750526d1ab
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-07 20:03:55 -0700

    WIP. controls. fix InputField async valtio update

--- src/components/2-main/panel-props/props/panel-field/index.tsx
+++ src/components/2-main/panel-props/props/panel-field/index.tsx
@@ -4,7 +4,7 @@ import { SrcriptItemField } from "@/store";
 import { propsBoxClasses, InputField } from "../../ui";
 
 export function PropsField({ item, ...rest }: { item: SrcriptItemField; } & HTMLAttributes<HTMLElement>) {
-    const snap = useSnapshot(item);
+    const snap = useSnapshot(item, { sync: true });
     return (
         <div className={propsBoxClasses} {...rest}>
             <InputField label="Field label" value={`${snap.id}`} onChange={(e) => item.id = e.target.value} />
