be3aceeed06e924155667d1142aea932a2ba1504
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-07 20:03:32 -0700

    WIP. controls. fix InputField props

--- src/components/2-main/panel-props/ui/input-field.tsx
+++ src/components/2-main/panel-props/ui/input-field.tsx
@@ -7,7 +7,13 @@ border-primary-400 border dark:border-none \
 bg-primary-100 dark:bg-primary-700/50 rounded \
 ";
 
-export function InputField({ label, horizontal = false, className, labelClasses, ...rest }: { label: string; horizontal?: boolean; labelClasses?: string; } & InputHTMLAttributes<HTMLInputElement>) {
+type InputFieldProps = {
+    label: string;
+    horizontal?: boolean;
+    labelClasses?: string;
+};
+
+export function InputField({ label, horizontal = false, className, labelClasses, ...rest }: InputFieldProps & InputHTMLAttributes<HTMLInputElement>) {
     return (
         <label className={classNames("flex", horizontal ? "items-center space-x-2" : "flex-col space-y-1")}>
             <div className={classNames("text-xs", labelClasses)}>{label}</div>
