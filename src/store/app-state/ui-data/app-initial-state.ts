6dd95561d12b9f458968bed53b99c41bb43ee291
Max Zakharzhevskiy <maxzz@msn.com>
2023-08-09 20:25:59 -0700

    WIP. app. fix tailwind dark mode switch; now on html document

--- src/store/app-state/ui-data/app-initial-state.ts
+++ src/store/app-state/ui-data/app-initial-state.ts
@@ -1,7 +1,8 @@
 import { UiState } from ".";
 
 export function setAppDarkMode(isDark: boolean) {
-    document.getElementsByTagName('body')[0].classList[isDark ? 'add' : 'remove']('dark');
+    //document.getElementsByTagName('body')[0].classList[isDark ? 'add' : 'remove']('dark');
+    document.getElementsByTagName('html')[0].classList[isDark ? 'add' : 'remove']('dark');
 }
 
 export function initializeUiState(initialUiState: UiState) {
