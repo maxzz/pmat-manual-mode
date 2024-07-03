0f75189e91aa7de33c5c9d3bfc21f4bb2bc45062
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-03 15:41:58 -0700

    WIP. state. update moveScriptCursor()

--- src/store/app-state/client-data/script-item-ops/3-move-script-cursor.ts
+++ src/store/app-state/client-data/script-item-ops/3-move-script-cursor.ts
@@ -1,14 +1,15 @@
 import { gEditorState } from "../0-all";
 
 export function moveScriptCursor(key: string) {
+    let selectedIdx = gEditorState.selectedIdx;
     if (gEditorState.itemMetas.length) {
         switch (key) {
             case 'ArrowUp': {
-                (gEditorState.selectedIdx > 0) && gEditorState.selectedIdx--;
+                (selectedIdx > 0) && gEditorState.selectedIdx--;
                 break;
             }
             case 'ArrowDown': {
-                (gEditorState.selectedIdx < gEditorState.itemMetas.length - 1) && gEditorState.selectedIdx++;
+                (selectedIdx < gEditorState.itemMetas.length - 1) && gEditorState.selectedIdx++;
                 break;
             }
             case 'Home': {
