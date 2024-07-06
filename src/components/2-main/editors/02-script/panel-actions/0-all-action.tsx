184ade7574e84bd9d7f2dbf01ae55d559bfb9a5a
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-06 14:49:48 -0700

    WIP. state. fix use of useAtomCallback

--- src/components/2-main/editors/02-script/panel-actions/0-all-action.tsx
+++ src/components/2-main/editors/02-script/panel-actions/0-all-action.tsx
@@ -1,23 +1,22 @@
+import { useCallback, useEffect } from "react";
+import { Getter, Setter } from "jotai";
 import { useAtomCallback } from "jotai/utils";
 import { PanelActionsTitle } from "./1-panel-title";
 import { PanelActionsList } from "./2-panel-items";
-import { useCallback } from "react";
-import { Getter } from "jotai";
-import { Setter } from "jotai/experimental";
 import { selectedIdxAtom, selectItemAtom } from "@/store";
 
 export function PanelActions() {
 
-    useAtomCallback(
-        () => {
-            useCallback(
-                (get: Getter, set: Setter) => {
-                    console.log('PanelActions mounted');
-                    set(selectItemAtom, get(selectedIdxAtom), true);
-                }, []
-            );
-        }, {}
+    const cb = useAtomCallback(
+        useCallback(
+            (get: Getter, set: Setter) => {
+                console.log('PanelActions mounted');
+                set(selectItemAtom, get(selectedIdxAtom), true);
+            }, []
+        )
     );
+
+    useEffect(() => { cb(); }, []);
 
     return (
         <div className="h-full min-h-[20rem] flex flex-col space-y-1 select-none">
