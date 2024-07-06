76f9cf0087c813ec9245835248206e96cb5eb8ba
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-06 14:44:06 -0700

    WIP. state. add useAtomCallback but useless, has get/set and no initial call

--- src/components/2-main/editors/02-script/panel-actions/0-all-action.tsx
+++ src/components/2-main/editors/02-script/panel-actions/0-all-action.tsx
@@ -1,11 +1,28 @@
+import { useAtomCallback } from "jotai/utils";
 import { PanelActionsTitle } from "./1-panel-title";
 import { PanelActionsList } from "./2-panel-items";
+import { useCallback } from "react";
+import { Getter } from "jotai";
+import { Setter } from "jotai/experimental";
+import { selectedIdxAtom, selectItemAtom } from "@/store";
 
 export function PanelActions() {
+
+    useAtomCallback(
+        () => {
+            useCallback(
+                (get: Getter, set: Setter) => {
+                    console.log('PanelActions mounted');
+                    set(selectItemAtom, get(selectedIdxAtom), true);
+                }, []
+            );
+        }, {}
+    );
+
     return (
         <div className="h-full min-h-[20rem] flex flex-col space-y-1 select-none">
             <PanelActionsTitle />
             <PanelActionsList />
         </div>
     );
-}
+};
