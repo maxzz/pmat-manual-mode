0909d6963dae39d20b31be7638bf92957f224681
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-06 16:29:34 -0700

    WIP. modifiers. add Frame component

--- src/components/2-main/editors/02-script/panel-props/props/panel-key/3-input-modifiers.tsx
+++ src/components/2-main/editors/02-script/panel-props/props/panel-key/3-input-modifiers.tsx
@@ -1,3 +1,4 @@
+import { HTMLAttributes, ReactNode } from "react";
 import { useSnapshot } from "valtio";
 import { SrcriptItemKey, SrcriptItemModifiers, modifierKeys } from "@/store";
 import { classNames } from "@/utils";
@@ -7,9 +8,27 @@ function Modifier({ label, name, item }: { label: string; name: SrcriptItemModif
     const snap = useSnapshot(item);
     return (
         <div className={classNames("max-w-[10rem] flex items-center space-x-2")}>
-            <div className="min-w-[8ch] text-xs">{label}</div>
-            <InputSelectUi items={modifierKeys} value={`${snap[name]}`} onValueChange={(value) => item[name] = +value} />
-            {/* <input className={classNames("px-2 py-1 bg-primary-700/50 rounded", focusClasses)} /> */}
+            <div className="min-w-[8ch] text-xs">
+                {label}
+            </div>
+
+            <InputSelectUi
+                items={modifierKeys}
+                value={`${snap[name]}`}
+                onValueChange={(value) => item[name] = +value}
+            />
+        </div>
+    );
+}
+
+export function Frame({ title, children }: HTMLAttributes<HTMLDivElement> & { title: string; children: ReactNode; }) {
+    return (
+        <div className="relative p-2 border-primary-400 dark:border-primary-700 border rounded flex flex-col space-y-1">
+            <div className="absolute left-1 -top-2.5 px-1 bg-primary-100 dark:bg-primary-800">
+                {title}
+            </div>
+
+            {children}
         </div>
     );
 }
@@ -17,15 +36,11 @@ function Modifier({ label, name, item }: { label: string; name: SrcriptItemModif
 export function InputModifiers({ item }: { item: SrcriptItemKey; }) {
     return (
         <div className="-mx-2 pt-3">
-            <div className="p-2 border-primary-400 dark:border-primary-700 border rounded flex flex-col space-y-1 relative">
-                <div className="absolute left-1 -top-2.5 px-1 bg-primary-100 dark:bg-primary-800">
-                    Key modifiers
-                </div>
-
+            <Frame title="Key modifiers">
                 <Modifier label="Shift" name="shift" item={item} />
                 <Modifier label="Control" name="ctrl" item={item} />
                 <Modifier label="Alt" name="alt" item={item} />
-            </div>
+            </Frame>
         </div>
     );
 }
