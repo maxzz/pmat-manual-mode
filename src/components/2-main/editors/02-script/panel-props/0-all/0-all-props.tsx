b4bb408d9fa9091716b4e6a377b9268808797355
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-06 19:21:52 -0700

    WIP. PanelProps. simplify editor layout

--- src/components/2-main/editors/02-script/panel-props/0-all/0-all-props.tsx
+++ src/components/2-main/editors/02-script/panel-props/0-all/0-all-props.tsx
@@ -1,6 +1,29 @@
 import { editorFrameClasses, focusClasses } from "@/components/ui/shared-styles";
-import { ItemPropsEditor } from "./2-item-props-editor";
 import { classNames } from "@/utils";
+import { gScriptState, selectedIdxAtom } from "@/store";
+import { useAtomValue } from "jotai";
+import { useSnapshot } from "valtio";
+import { getPropsEditor } from "../props";
+import { InPanelPropsTitle } from "./2-editor-title";
+
+function ItemPropsEditor() {
+    const { scriptItems: scriptItemsSnap } = useSnapshot(gScriptState);
+
+    const selectedRef = useAtomValue(selectedIdxAtom);
+
+    const scriptItemSnap = scriptItemsSnap[selectedRef];
+    if (!scriptItemSnap) {
+        return <InPanelPropsTitle />;
+    }
+
+    const scriptItem = gScriptState.scriptItems[selectedRef];
+    const propsEditor = getPropsEditor({ scriptItem, scriptItemSnap });
+
+    return (<>
+        <InPanelPropsTitle type={scriptItemSnap.type} />
+        {propsEditor}
+    </>);
+}
 
 export function PanelProps() {
     return (
