d9fc58f28f54d1cf9d1115d323c74c805d5e23cd
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-06 17:40:18 -0700

    WIP. ScriptEditor. update container items size

--- src/components/2-main/editors/02-script/index.tsx
+++ src/components/2-main/editors/02-script/index.tsx
@@ -3,8 +3,8 @@ import { PanelProps } from './panel-props';
 
 export function ScriptEditor() {
     return (
-        <div className="p-4 max-w-[720px] flex flex-col gap-2">
-            <div className="h-full grid grid-cols-[minmax(330px,1fr),minmax(200px,1fr)] gap-4">
+        <div className="p-4 max-w-3xl flex flex-col gap-2">
+            <div className="h-full grid grid-cols-[minmax(250px,1fr),minmax(180px,1fr)] gap-4">
                 <PanelActions />
                 <PanelProps />
             </div>
