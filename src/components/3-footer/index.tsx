b73b706ee5c27ad9faa2915a0ea3fa2eeb1c3c11
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-09 18:32:24 -0700

    WIP. ColorModeSwitch. update github link

--- src/components/3-footer/index.tsx
+++ src/components/3-footer/index.tsx
@@ -2,6 +2,9 @@ import { useSnapshot } from 'valtio';
 import { appUi, setAppDarkMode } from '@/store';
 import { DarkLightSwitch } from '../ui/icons/DarkLight';
 import { IconSunnyvale } from '../ui/icons';
+import { classNames } from '@/utils';
+
+const colorModeSwitchClasses = "hover:bg-primary-300 dark:hover:bg-primary-700 active:scale-[.97] rounded cursor-pointer";
 
 function ColorModeSwitch() {
     const { darkMode } = useSnapshot(appUi.uiState);
@@ -12,7 +15,7 @@ function ColorModeSwitch() {
     }
 
     return (
-        <div className="absolute p-1.5 right-2 hover:bg-primary-300 dark:hover:bg-primary-700 active:scale-[.97] rounded cursor-pointer">
+        <div className={classNames("absolute p-1.5 right-2", colorModeSwitchClasses)}>
             <DarkLightSwitch className="size-4" isDark={darkMode} title="Light/Dark Mode Switch" onClick={changeMode} />
         </div>
     );
@@ -20,8 +23,11 @@ function ColorModeSwitch() {
 
 export function SectionFooter() {
     return (
-        <div className="relative px-2 py-3 flex items-center justify-center border-primary-500/20 border-t">
-            <a href="https://github.com/maxzz/pmat-manual-mode" target='_blank'><IconSunnyvale className="w-4 h-4" /></a>
+        <div className="relative px-2 py-3 border-primary-500/20 border-t flex items-center justify-start">
+            <a href="https://github.com/maxzz/pmat-manual-mode" target='_blank'>
+                <IconSunnyvale className="size-4 text-primary-500" />
+            </a>
+
             <ColorModeSwitch />
         </div>
     );
