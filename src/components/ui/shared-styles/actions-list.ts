2825f4134b83794adec4a8d7933d113e0570f744
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-09 18:05:12 -0700

    WIP. fix PanelActionsList; fix current item hover

--- src/components/ui/shared-styles/actions-list.ts
+++ src/components/ui/shared-styles/actions-list.ts
@@ -1,3 +1,5 @@
+export const rowParentActiveClasses = "[--parent-active:0px] focus-within:[--parent-active:1px]";
+
 export const rowClasses = "\
 leading-6 \
 hover:bg-primary-400/30 dark:hover:bg-primary-600/30";
@@ -6,11 +8,10 @@ export const rowSelectedClasses = "\
 text-primary-800 dark:text-primary-200 \
 \
 bg-primary-400/20 dark:bg-primary-400/20 \
+hover:!bg-primary-400/30 \
 \
 outline-primary-400 \
 \
-[outline-width:var(--parent-act)] \
+[outline-width:var(--parent-active)] \
 \
 outline rounded-[3px] cursor-default";
-
-export const rowParentActiveClasses = "[--parent-act:0px] focus-within:[--parent-act:1px]";
