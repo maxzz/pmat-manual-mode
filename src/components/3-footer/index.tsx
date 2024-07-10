b81f0a1c223101986a018bc914098b83f5ba64c6
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-09 18:27:47 -0700

    WIP. ColorModeSwitch. update feedback classes

--- src/components/3-footer/index.tsx
+++ src/components/3-footer/index.tsx
@@ -12,13 +12,15 @@ function ColorModeSwitch() {
     }
 
     return (
-        <DarkLightSwitch className="absolute right-3 w-4 h-4 cursor-pointer" isDark={darkMode} title="Light/Dark Mode Switch" onClick={changeMode} />
+        <div className="absolute p-1.5 right-2 hover:bg-primary-300 dark:hover:bg-primary-700 active:scale-[.97] rounded cursor-pointer">
+            <DarkLightSwitch className="size-4" isDark={darkMode} title="Light/Dark Mode Switch" onClick={changeMode} />
+        </div>
     );
 }
 
 export function SectionFooter() {
     return (
-        <div className="relative p-2 flex items-center justify-center">
+        <div className="relative px-2 py-3 flex items-center justify-center border-primary-500/20 border-t">
             <a href="https://github.com/maxzz/pmat-manual-mode" target='_blank'><IconSunnyvale className="w-4 h-4" /></a>
             <ColorModeSwitch />
         </div>
