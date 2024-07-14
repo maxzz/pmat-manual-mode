eb135d29c430f45826fba7e181125d93788ef556
Max Zakharzhevskiy <maxzz@msn.com>
2024-07-13 17:06:11 -0700

    WIP. manual mode definitions. new actionKeys format. 4. back to double def format

--- src/components/2-main/editors/02-script/panel-props/props/panel-key/1-input-key.tsx
+++ src/components/2-main/editors/02-script/panel-props/props/panel-key/1-input-key.tsx
@@ -2,13 +2,11 @@ import { useSnapshot } from "valtio";
 import { SrcriptItemKey, actionKeys } from "@/store";
 import { InputSelect } from "../../ui";
 
-const keyNames = actionKeys.map(([diplayName]) => diplayName);
-
 export function InputKey({ item }: { item: SrcriptItemKey; }) {
     const snap = useSnapshot(item);
     return (
         <InputSelect
-            items={keyNames}
+            items={actionKeys}
             label="Key"
             labelClasses="min-w-[9ch]"
             horizontal
